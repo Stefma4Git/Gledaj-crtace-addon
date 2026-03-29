28c6faf586de1a1b91d8b1ea3775dcdc41ab0e70
Stefma <60974403+Stefma4Git@users.noreply.github.com>
2026-03-29 14:36:18 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,17 +1,11 @@
-// logger.js
-const winston = require('winston');
+const { createLogger, transports, format } = require("winston");
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} ${level}: ${message}`;
-        })
-    ),
-    transports: [
-        new winston.transports.Console()
-    ]
+module.exports = createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: format.combine(
+      format.timestamp(),
+      format.colorize(),
+      format.printf(({ level, message, timestamp }) => `${timestamp} ${level}: ${message}`)
+  ),
+  transports: [ new transports.Console()  ],
 });
-
-module.exports = logger;
